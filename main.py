b1112e64223180c37ded087f00cd8d59033845b3
SnakeBlanket <me@bainble.kr>
2024-08-24 18:03:32 +0900

    feat(main): add cors setting

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import dotenv
 import fastapi
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 dotenv.load_dotenv(verbose=True)
 
 app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
