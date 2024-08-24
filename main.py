829618a0d265d8a211bb1bba95a568c6368b8e1c
SnakeBlanket <me@bainble.kr>
2024-08-24 14:01:19 +0900

    BaseCode Upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import dotenv
+import fastapi
+import uvicorn
+
+dotenv.load_dotenv(verbose=True)
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
